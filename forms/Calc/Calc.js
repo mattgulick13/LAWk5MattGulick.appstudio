ca78be91b62c5224085f35849913ac2609fd5017
mattgulick13 <90654494+mattgulick13@users.noreply.github.com>
2021-09-22 16:42:07 -0500

    Initial commit

---
+++ forms/Calc/Calc.js
@@ -0,0 +1,15 @@
+/*
+function Calculate(num1, num2){
+  let answer = (num1+num2)
+  return answer
+  }
+
+let number1 = Number(prompt("Enter a whole number:"))
+let number2 = Number(prompt("Enter a second whole number:"))
+
+num1=parseInt(number1)
+num2=parseInt(number2)
+
+let newAnswer = Calculate(number1, number2)
+console.log(`The sum of ${number1} and ${number2} is ${newAnswer}.`)
+*/
---
+++ forms/While/While.js
@@ -0,0 +1,9 @@
+/*
+let wantToContinue = true
+let i = 1
+while (i = 1) {
+  console.log(`This is loop iteration 1.`)
+  wantToContinue=confirm('Do you want to run the loop again? Press OK for yes, Cancel for no.')
+  i = i ++
+}
+*/
